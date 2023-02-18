46898ff67eb6c1b756ccd94aebb1ffa92500ddb9
rising-jun <jun36817@naver.com>
2023-02-18 14:45:18 +0900

    Initial Commit

---
+++ Drawing-Clean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drawing-Clean
+//
+//  Created by 김동준 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
